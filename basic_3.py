dee0d3eeef4600177b11adb76a702ab44ed4a721
Sydney Chang <syd2chang@gmail.com>
2024-05-01 16:46:46 -0700

    top down pass + bug fixes

--- basic_3.py
+++ basic_3.py
@@ -31,6 +31,15 @@ def inputStringGenerator() -> list:           #reads from txt file and creates t
 
     return [inputString1, inputString2]
 
+def bottomUpPass() -> None:
+    for j in range(1, string2Len + 1):                                               
+        for i in range(1, string1Len + 1):
+            memoizedArray[j][i] = min(
+                memoizedArray[j-1][i-1] + mismatchCosts[inputString2[j-1]][inputString1[i-1]],
+                memoizedArray[j-1][i] + delta,
+                memoizedArray[j][i-1] + delta
+            )
+
 if __name__ == "__main__":             #main driver
     inputs = inputStringGenerator()
     inputString1 = inputs[0]
@@ -39,29 +48,58 @@ if __name__ == "__main__":             #main driver
     string1Len = len(inputString1)
     string2Len = len(inputString2)
 
-    #print(inputs[0] + "\n" + inputs[1])
+    print(inputs[0] + "\n" + inputs[1])
     
-    memoizedArray = [[0 for x in range(string1Len + 1)] for y in range(string2Len + 1)]      # x = string1 length, y = string2 length
+    memoizedArray = [[0 for x in range(string1Len + 1)] for y in range(string2Len + 1)]      #x = string1 length, y = string2 length
+    
     for i in range(string1Len + 1):
         memoizedArray[0][i] = i * delta
-    
+
     for i in range(string2Len + 1):
         memoizedArray[i][0] = i * delta
 
-    for i in range(1, string1Len + 1):                                                    #bottom up pass
-        for j in range(1, string2Len + 1):
-            memoizedArray[i][j] = min(
-                memoizedArray[i-1][j-1] + mismatchCosts[inputString1[i-1]][inputString2[j-1]],
-                memoizedArray[i-1][j] + delta,
-                memoizedArray[i][j-1] + delta
-            )
+    bottomUpPass()
 
+    m, n = string1Len, string2Len
+    costOfAlignment = memoizedArray[n][m]                             #value of optimal solution
+    alignedString1, alignedString2 = "", ""
+    '''while(m > 0 or n > 0): 
+        if memoizedArray[n][m] == memoizedArray[n-1][m-1] + mismatchCosts[inputString1[m-1]][inputString2[n-1]]:
+            alignedString1 += inputString1[m-1] 
+            alignedString2 += inputString2[n-1]
+            m -= 1   
+            n -= 1
+        elif memoizedArray[n][m] == memoizedArray[n][m-1] + delta:
+            alignedString1 += inputString1[m-1]
+            alignedString2 += "_"
+            m -= 1
+        else:
+            alignedString1 += "_"
+            alignedString2 += inputString2[n-1]
+            n -= 1
+        valueOfOpt = memoizedArray[n][m]  #remove. for testing'''
+    
+    while(m > 0 or n > 0): 
+        if memoizedArray[n][m] == memoizedArray[n-1][m-1] + mismatchCosts[inputString1[m-1]][inputString2[n-1]]:
+            alignedString1 += inputString1[m-1] 
+            alignedString2 += inputString2[n-1]
+            m -= 1   
+            n -= 1
+        elif memoizedArray[n][m] == memoizedArray[n-1][m] + delta:
+            alignedString1 += "_"
+            alignedString2 += inputString2[n-1]
+            n -= 1
+        else: #memoizedArray[n][m] == memoizedArray[n][m-1] + delta:
+            alignedString1 += inputString1[m-1]
+            alignedString2 += "_"
+            m -= 1
 
+    print(costOfAlignment)
+    print("".join(reversed(alignedString1)))
+    print("".join(reversed(alignedString2)))
 
     #scrappable code - just to output opt array in a readable form
     f = open(os.path.join(cwd, "memoizedArrayOutput"), "w")
     for row in memoizedArray: 
         f.write(", ".join(str(x) for x in row) + "\n")
     f.close()
-
-
